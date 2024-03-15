8ab043cdfba1da23ab85dcea13e84f48e702eceb
al-badarin <jamalalbadarin@gmail.com>
2024-03-16 00:35:39 +0200

    Update recipe-list.component.ts

--- recipe-sharing-app/src/app/recipe-list/recipe-list.component.ts
+++ recipe-sharing-app/src/app/recipe-list/recipe-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecipeService } from '../services/recipe.service';
+import { Recipe } from '../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [];
 
+  constructor(private recipeService: RecipeService) {}
+
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
+  }
 }
