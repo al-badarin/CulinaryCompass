25dcd82b4cd679e1c851330f2d948a53151dc92f
al-badarin <jamalalbadarin@gmail.com>
2024-03-17 00:50:51 +0200

    removed unnecessary components

--- recipe-sharing-app/src/app/app-routing.module.ts
+++ recipe-sharing-app/src/app/app-routing.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
-import { RecipeFormComponent } from './recipe-form/recipe-form.component';
 import { HomeComponent } from './core/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'recipe-list', component: RecipeListComponent },
   { path: 'recipes/:id', component: RecipeDetailsComponent },
-  { path: 'add-recipe', component: RecipeFormComponent },
-  { path: 'edit-recipe/:id', component: RecipeFormComponent },
   
   //TODO: ADD ERROR PAGE 404!:
   // { path: '**', redirectTo: '' } // For now, it redirects to home for unknown paths
--- recipe-sharing-app/src/app/app.module.ts
+++ recipe-sharing-app/src/app/app.module.ts
@@ -3,18 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
-import { RecipeFormComponent } from './recipe-form/recipe-form.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipesComponent,
     RecipeDetailsComponent,
-    RecipeFormComponent,
     RecipeListComponent,
   ],
   imports: [
