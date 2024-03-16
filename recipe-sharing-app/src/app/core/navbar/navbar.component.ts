3125aefddad5b05edff2884058f5aadcca90ddca
al-badarin <jamalalbadarin@gmail.com>
2024-03-17 01:30:41 +0200

    updated navbar wit click func for routing

--- recipe-sharing-app/src/app/core/navbar/navbar.component.ts
+++ recipe-sharing-app/src/app/core/navbar/navbar.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css'],
+  styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  isNavbarOpen = false;
+
+  constructor(private router: Router) {}
+
+  toggleNavbar(): void {
+    this.isNavbarOpen = !this.isNavbarOpen;
+  }
+
+  navigateTo(route: string): void {
+    this.router.navigateByUrl(route);
+    this.isNavbarOpen = false; // Close the navbar after navigation
+  }
+}
