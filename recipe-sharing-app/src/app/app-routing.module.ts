0f2ed37c0249626845e5f43e7e1437d45c7f1bd9
al-badarin <jamalalbadarin@gmail.com>
2024-03-17 00:29:33 +0200

    updated recipe-list comp / routing updates

--- recipe-sharing-app/src/app/app-routing.module.ts
+++ recipe-sharing-app/src/app/app-routing.module.ts
@@ -7,12 +7,13 @@ import { HomeComponent } from './core/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'recipe-list', component: RecipeListComponent },
   { path: 'recipes/:id', component: RecipeDetailsComponent },
   { path: 'add-recipe', component: RecipeFormComponent },
   { path: 'edit-recipe/:id', component: RecipeFormComponent },
   
   //TODO: ADD ERROR PAGE 404!:
-  { path: '**', redirectTo: '' } // For now, it redirects to home for unknown paths
+  // { path: '**', redirectTo: '' } // For now, it redirects to home for unknown paths
 ];
 
 
