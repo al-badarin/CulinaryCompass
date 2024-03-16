80264d7a74ffe8705e6cc2ed465c8523e3ca1c1b
al-badarin <jamalalbadarin@gmail.com>
2024-03-17 00:43:57 +0200

    Update recipe-details.component.ts

--- recipe-sharing-app/src/app/recipe-details/recipe-details.component.ts
+++ recipe-sharing-app/src/app/recipe-details/recipe-details.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../models/recipe.model';
+import { ActivatedRoute } from '@angular/router';
+import { RecipeService } from '../services/recipe.service';
 
 @Component({
   selector: 'app-recipe-details',
   templateUrl: './recipe-details.component.html',
-  styleUrls: ['./recipe-details.component.css']
+  styleUrls: ['./recipe-details.component.css'],
 })
-export class RecipeDetailsComponent {
+export class RecipeDetailsComponent implements OnInit {
+  recipe: Recipe | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      const recipeId = params.get('id');
+      if (recipeId) {
+        this.recipe = this.recipeService.getRecipesById(recipeId);
+      }
+    });
+  }
 }
