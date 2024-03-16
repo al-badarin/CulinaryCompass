479f00750d51bb4eb2e78e2586dbb8cdb2f3fb2c
al-badarin <jamalalbadarin@gmail.com>
2024-03-17 01:07:04 +0200

    added AboutComponent

--- recipe-sharing-app/src/app/app-routing.module.ts
+++ recipe-sharing-app/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 import { HomeComponent } from './core/home/home.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'recipe-list', component: RecipeListComponent },
+  { path: 'recipes', component: RecipeListComponent },
   { path: 'recipes/:id', component: RecipeDetailsComponent },
+  { path: 'about', component: AboutComponent },
   
   //TODO: ADD ERROR PAGE 404!:
   // { path: '**', redirectTo: '' } // For now, it redirects to home for unknown paths
