3be75e2d8951b3aaf89671206b04bf367f939e63
al-badarin <jamalalbadarin@gmail.com>
2024-03-17 23:36:50 +0200

    RouterModule imported to core.module.ts

--- recipe-sharing-app/src/app/core/core.module.ts
+++ recipe-sharing-app/src/app/core/core.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HomeComponent, FooterComponent, NavbarComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [HomeComponent, FooterComponent, NavbarComponent],
 })
 export class CoreModule {}
