c850cfe30da5feea99ae22f64c39626f748e438d
al-badarin <jamalalbadarin@gmail.com>
2024-03-17 23:37:02 +0200

    Update home.component.ts

--- recipe-sharing-app/src/app/core/home/home.component.ts
+++ recipe-sharing-app/src/app/core/home/home.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/models/recipe.model';
+import { RecipeService } from 'src/app/services/recipe.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  recipes: Recipe[] = [];
 
+  constructor(private recipeService: RecipeService) {}
+
+  ngOnInit(): void {
+    this.fetchRecipes();
+  }
+
+  fetchRecipes(): void {
+    this.recipeService.getRecipes().subscribe(
+      (recipes) => {
+        this.recipes = recipes;
+      },
+      (error) => {
+        console.error('Error fetching recipes:', error);
+      }
+    );
+  }
 }
