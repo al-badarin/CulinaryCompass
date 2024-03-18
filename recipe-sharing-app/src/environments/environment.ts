0908834093927fafebff363c9afe154906bb2a31
al-badarin <jamalalbadarin@gmail.com>
2024-03-19 01:08:54 +0200

    npm i @angular/firebase

---
+++ recipe-sharing-app/src/environments/environment.ts
@@ -0,0 +1,14 @@
+// export const environment = {
+//   production: false,
+//   firebaseConfig: {
+//     apiKey: 'AIzaSyCY8wvX_akAxkpn9722YZ6lXOl8Ps93b7g',
+//     authDomain: 'recipe-sharing-app-al-badarin.firebaseapp.com',
+//     databaseURL:
+//       'https://recipe-sharing-app-al-badarin-default-rtdb.europe-west1.firebasedatabase.app',
+//     projectId: 'recipe-sharing-app-al-badarin',
+//     storageBucket: 'recipe-sharing-app-al-badarin.appspot.com',
+//     messagingSenderId: '649685524410',
+//     appId: '1:649685524410:web:9eed055d88f16aa159c783',
+//     measurementId: 'G-Y2ZE9H6K0X',
+//   },
+// };
