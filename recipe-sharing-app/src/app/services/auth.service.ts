b6c73d0b96e53a181ba9dae1ba1ea2a943766e6c
al-badarin <jamalalbadarin@gmail.com>
2024-03-20 01:27:26 +0200

    Update auth.service.ts

--- recipe-sharing-app/src/app/services/auth.service.ts
+++ recipe-sharing-app/src/app/services/auth.service.ts
@@ -1,15 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { User } from '../models/user';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  // TODO: MOVE THE URL TO ENVIRONMENT.TS
   private baseUrl = 'http://localhost:3000/api';
 
-  constructor(private http: HttpClient) {}
+  // user: User | undefined;
+  // USER_KEY = '[user]';
 
+  constructor(private http: HttpClient, private router: Router) {}
+
+  // Check if user is logged in
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('token');
+  }
+
+  // ** IMPLEMENTED BY ME **
   register(data: any): Observable<any> {
     return this.http.post<any>(`${this.baseUrl}/register`, data);
   }
@@ -23,8 +35,9 @@ export class AuthService {
   }
 
   logout(): Observable<any> {
+    localStorage.removeItem('token');
     return this.http.post<any>(`${this.baseUrl}/logout`, {});
-  }
+  } 
 
   getProfileInfo(): Observable<any> {
     return this.http.get<any>(`${this.baseUrl}/profile`);
@@ -33,4 +46,7 @@ export class AuthService {
   editProfileInfo(data: any): Observable<any> {
     return this.http.put<any>(`${this.baseUrl}/profile`, data);
   }
+
+  // *** IMPLEMENTED FROM WORKSHOP ***
+  //...
 }
