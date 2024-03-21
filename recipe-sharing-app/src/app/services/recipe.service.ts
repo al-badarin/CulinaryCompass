a9788190a045442a4f4ea83033034d6b9ae0adb8
al-badarin <jamalalbadarin@gmail.com>
2024-03-21 17:20:52 +0200

    Update recipe.service.ts

--- recipe-sharing-app/src/app/services/recipe.service.ts
+++ recipe-sharing-app/src/app/services/recipe.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Recipe } from '../models/recipe.model';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Recipe } from '../models/recipe.model';
 import { FirestoreRecipe } from '../models/FirestoreRecipe.model';
 
 @Injectable({
@@ -11,53 +12,87 @@ export class RecipeService {
   private baseUrl =
     'https://firestore.googleapis.com/v1/projects/recipe-sharing-app-al-badarin/databases/(default)/documents/recipes';
 
-  
-
   constructor(private http: HttpClient) {}
 
   getRecipes(): Observable<Recipe[]> {
-    return this.http.get<any>(this.baseUrl).pipe(
+    return this.http.get<FirestoreRecipe>(this.baseUrl).pipe(
       map((response) => {
-        const recipes: Recipe[] = response.documents.map(
-          (doc: FirestoreRecipe) => {
-            const data = doc.fields;
-            return {
-              id: doc.name.split('/').pop(),
-              title: data.title.stringValue,
-              description: data.description.stringValue,
-              imageUrl: data.imageUrl.stringValue,
-              ingredients: data.ingredients.arrayValue.values.map(
-                (ingredient) => ingredient.stringValue
-              ),
-              instructions: data.instructions.arrayValue.values.map(
-                (instruction) => instruction.stringValue
-              ),
-            };
-          }
-        );
-        return recipes;
+        if (
+          !response ||
+          !response.documents ||
+          !Array.isArray(response.documents)
+        ) {
+          console.error('Invalid response format:', response);
+          return [];
+        }
+
+        return response.documents.map((doc: FirestoreRecipe) => {
+          const data = doc.fields;
+          return {
+            id: doc.name.split('/').pop() || '',
+            title: data.title.stringValue || '',
+            description: data.description.stringValue || '',
+            imageUrl: data.imageUrl.stringValue || '',
+            ingredients: (data.ingredients.arrayValue.values || []).map(
+              (ingredient: { stringValue: string }) =>
+                ingredient.stringValue || ''
+            ),
+            instructions: (data.instructions.arrayValue.values || []).map(
+              (instruction: { stringValue: string }) =>
+                instruction.stringValue || ''
+            ),
+          };
+        });
       })
     );
   }
 
   getRecipeDetails(recipeId: string): Observable<Recipe | undefined> {
     const url = `${this.baseUrl}/${recipeId}`;
-    return this.http.get<any>(url).pipe(
+    // return this.http.get<Recipe>(url);
+
+    return this.http.get<FirestoreRecipe>(url).pipe(
       map((response) => {
         const data = response.fields;
         return {
-          id: response.name.split('/').pop(),
-          title: data.title.stringValue,
-          description: data.description.stringValue,
-          imageUrl: data.imageUrl.stringValue,
-          ingredients: data.ingredients.arrayValue.values.map(
-            (ingredient: { stringValue: string }) => ingredient.stringValue
+          id: response.name.split('/').pop() || '',
+          title: data.title.stringValue || '',
+          description: data.description.stringValue || '',
+          imageUrl: data.imageUrl.stringValue || '',
+          ingredients: (data.ingredients.arrayValue.values || []).map(
+            (ingredient: { stringValue: string }) =>
+              ingredient.stringValue || ''
           ),
-          instructions: data.instructions.arrayValue.values.map(
-            (instruction: { stringValue: string }) => instruction.stringValue
+          instructions: (data.instructions.arrayValue.values || []).map(
+            (instruction: { stringValue: string }) =>
+              instruction.stringValue || ''
           ),
         };
       })
     );
+  }
+
+  addRecipe(recipe: Recipe): Observable<Recipe> {
+    return this.http.post<Recipe>(this.baseUrl, {
+      fields: {
+        title: { stringValue: recipe.title },
+        description: { stringValue: recipe.description },
+        imageUrl: { stringValue: recipe.imageUrl },
+        ingredients: {
+          arrayValue: {
+            values: recipe.ingredients.map((ingredient) => ({
+              stringValue: ingredient,
+            })),
+          },
+        },
+        instructions: {
+          arrayValue: {
+            values: recipe.instructions.map((instruction) => ({
+              stringValue: instruction,
+            })),
+          },
+        },
+      },
+    });
   }
 }
