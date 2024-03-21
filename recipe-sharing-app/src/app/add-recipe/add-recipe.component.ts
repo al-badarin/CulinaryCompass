19c2ff4fcbe8384bc484d1b1f131d8e8199dde22
al-badarin <jamalalbadarin@gmail.com>
2024-03-21 17:21:09 +0200

    Update add-recipe.component.ts

--- recipe-sharing-app/src/app/add-recipe/add-recipe.component.ts
+++ recipe-sharing-app/src/app/add-recipe/add-recipe.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { Recipe } from '../models/recipe.model';
 import { RecipeService } from '../services/recipe.service';
-import { NgForm } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-add-recipe',
@@ -10,27 +14,72 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./add-recipe.component.css'],
 })
 export class AddRecipeComponent {
-  constructor(private recipeService: RecipeService, private router: Router) {}
+  recipeForm: FormGroup;
 
-  onSubmit(form: NgForm): void {
-    if (form.invalid) {
-      return;
-    }
-    // console.log('form values: ', form.value);
+  constructor(
+    private recipeService: RecipeService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.recipeForm = this.fb.group({
+      title: ['', Validators.required],
+      description: [''],
+      image: [''],
+      ingredients: this.fb.array([this.fb.control('')]),
+      instructions: this.fb.array([this.fb.control('')]),
+    });
+  }
 
-    const { title, description, image, ingredients, instructions } = form.value;
+  get ingredients() {
+    return this.recipeForm?.get('ingredients') as FormArray;
+  }
 
-    this.recipeService
-      .addRecipe(title, description, image, ingredients, instructions)
-      .subscribe(
+  get instructions() {
+    return this.recipeForm?.get('instructions') as FormArray;
+  }
+
+  addIngredient() {
+    this.ingredients.push(this.fb.control(''));
+  }
+
+  removeIngredient(index: number) {
+    this.ingredients.removeAt(index);
+  }
+
+  addInstruction() {
+    this.instructions.push(this.fb.control(''));
+  }
+
+  removeInstruction(index: number) {
+    this.instructions.removeAt(index);
+  }
+
+  onSubmit() {
+    if (this.recipeForm.valid) {
+      console.log('recipeForm value: ', this.recipeForm.value);
+
+      const recipeData = {
+        title: this.recipeForm.value.title,
+        description: this.recipeForm.value.description,
+        imageUrl: this.recipeForm.value.image,
+        ingredients: this.recipeForm.value.ingredients,
+        instructions: this.recipeForm.value.instructions,
+      };
+
+      console.log('recipeData: ', recipeData);
+
+      this.recipeService.addRecipe(recipeData).subscribe(
         (response) => {
           console.log('Recipe added successfully:', response);
-          form.reset();
+          this.recipeForm.reset();
           this.router.navigate(['/recipes']);
         },
         (error) => {
           console.error('Error adding recipe:', error);
         }
       );
+    } else {
+      return;
+    }
   }
 }
