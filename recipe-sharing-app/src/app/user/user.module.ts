4bb445b9b95ea0192c11c2c01d261609384ac7fa
al-badarin <jamalalbadarin@gmail.com>
2024-03-21 23:05:47 +0200

    Update user.module.ts

--- recipe-sharing-app/src/app/user/user.module.ts
+++ recipe-sharing-app/src/app/user/user.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { UserRoutingModule } from './user-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, UserRoutingModule, FormsModule, ReactiveFormsModule],
 })
 export class UserModule {}
