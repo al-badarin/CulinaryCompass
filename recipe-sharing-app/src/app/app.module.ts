e44029738ef1736d7c2745d474013ea5d740be0d
al-badarin <jamalalbadarin@gmail.com>
2024-03-21 22:20:25 +0200

    Update app.module.ts

--- recipe-sharing-app/src/app/app.module.ts
+++ recipe-sharing-app/src/app/app.module.ts
@@ -3,29 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
-import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { CoreModule } from './core/core.module';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ErrorPageComponent } from './error-page/error-page.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AddRecipeComponent } from './add-recipe/add-recipe.component';
+import { RecipeModule } from './recipe/recipe.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipeDetailsComponent,
-    RecipeListComponent,
     AboutComponent,
     ContactComponent,
     ErrorPageComponent,
     LoginComponent,
     RegisterComponent,
-    AddRecipeComponent,
   ],
   imports: [
     BrowserModule,
@@ -33,6 +28,7 @@ import { AddRecipeComponent } from './add-recipe/add-recipe.component';
     CoreModule,
     FormsModule,
     ReactiveFormsModule,
+    RecipeModule,
     AppRoutingModule,
   ],
   providers: [],
