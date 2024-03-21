ec956736738828ffdfc64cf148b981d395f3a947
al-badarin <jamalalbadarin@gmail.com>
2024-03-21 22:20:20 +0200

    Update home.component.ts

--- recipe-sharing-app/src/app/core/home/home.component.ts
+++ recipe-sharing-app/src/app/core/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from 'src/app/models/recipe.model';
-import { RecipeService } from 'src/app/services/recipe.service';
+import { RecipeService } from 'src/app/recipe/recipe.service';
 
 @Component({
   selector: 'app-home',
