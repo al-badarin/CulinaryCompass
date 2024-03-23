abdc095476b008ba72d335f1174391a97f24780e
al-badarin <jamalalbadarin@gmail.com>
2024-03-24 01:50:32 +0200

    Update recipe-list.component.ts

--- recipe-sharing-app/src/app/recipe/recipe-list/recipe-list.component.ts
+++ recipe-sharing-app/src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RecipeService } from '../recipe.service';
 import { Recipe } from '../../models/recipe.model';
-
+ 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
