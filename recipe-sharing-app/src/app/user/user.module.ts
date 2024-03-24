44169b4637d46d6503d63df67d8ba149a9358640
al-badarin <jamalalbadarin@gmail.com>
2024-03-24 23:53:21 +0200

    ProfileComponent declared in user module

--- recipe-sharing-app/src/app/user/user.module.ts
+++ recipe-sharing-app/src/app/user/user.module.ts
@@ -5,9 +5,10 @@ import { RegisterComponent } from './register/register.component';
 import { UserRoutingModule } from './user-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent],
+  declarations: [LoginComponent, RegisterComponent, ProfileComponent],
   imports: [
     CommonModule,
     UserRoutingModule,
