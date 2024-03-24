80d652b3e0168ad7de05c48e6cf8c8b7f183b8a4
al-badarin <jamalalbadarin@gmail.com>
2024-03-24 14:17:00 +0200

    Update authenticate.component.ts

--- recipe-sharing-app/src/app/authenticate/authenticate.component.ts
+++ recipe-sharing-app/src/app/authenticate/authenticate.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../user/auth.service';
 
 @Component({
   selector: 'app-authenticate',
   templateUrl: './authenticate.component.html',
-  styleUrls: ['./authenticate.component.css']
+  styleUrls: ['./authenticate.component.css'],
 })
-export class AuthenticateComponent {
+export class AuthenticateComponent implements OnInit {
+  isAuthenticating = true;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.getProfile().subscribe({
+      next: () => {
+        this.isAuthenticating = false;
+      },
+      error: () => {
+        this.isAuthenticating = false;
+      },
+      complete: () => {
+        this.isAuthenticating = false;
+      },
+    });
+  }
 }
