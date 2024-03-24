dc6f8f5637196b8f80c7522150600ed5c4e956ea
al-badarin <jamalalbadarin@gmail.com>
2024-03-24 23:53:51 +0200

    Update recipe.service.ts

--- recipe-sharing-app/src/app/recipe/recipe.service.ts
+++ recipe-sharing-app/src/app/recipe/recipe.service.ts
@@ -4,6 +4,7 @@ import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { Recipe } from '../models/recipe.model';
 import { FirestoreRecipe } from '../models/FirestoreRecipe.model';
+import { AuthService } from '../user/auth.service';
 
 @Injectable({
   providedIn: 'root',
@@ -12,8 +13,9 @@ export class RecipeService {
   private baseUrl =
     'https://firestore.googleapis.com/v1/projects/recipe-sharing-app-al-badarin/databases/(default)/documents/recipes';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
+  // GET ALL RECIPES
   getRecipes(): Observable<Recipe[]> {
     return this.http.get<FirestoreRecipe>(this.baseUrl).pipe(
       map((response) => {
@@ -53,6 +55,7 @@ export class RecipeService {
     );
   }
 
+  // GET RECIPE'S DETAILS
   getRecipeDetails(recipeId: string): Observable<Recipe | undefined> {
     const url = `${this.baseUrl}/${recipeId}`;
     // return this.http.get<Recipe>(url);
@@ -84,6 +87,7 @@ export class RecipeService {
     );
   }
 
+  // ADD A RECIPE
   addRecipe(recipe: Recipe): Observable<Recipe> {
     return this.http
       .post<Recipe>(this.baseUrl, {
@@ -115,5 +119,53 @@ export class RecipeService {
           );
         })
       );
+  }
+
+  // GET USER'S RECIPES BY ID
+  getUserRecipesById(): Observable<Recipe[]> {
+    const userId = this.authService.getUserId();
+
+    if (!userId) {
+      return throwError('User ID not found');
+    }
+
+    const url = `${this.baseUrl}?q=userId:${userId}`;
+
+    return this.http.get<FirestoreRecipe>(url).pipe(
+      map((response) => {
+        if (
+          !response ||
+          !response.documents ||
+          !Array.isArray(response.documents)
+        ) {
+          console.error('Invalid response format:', response);
+          return [];
+        }
+
+        return response.documents.map((doc: FirestoreRecipe) => {
+          const data = doc.fields;
+          return {
+            id: doc.name.split('/').pop() || '',
+            title: data.title.stringValue || '',
+            description: data.description.stringValue || '',
+            imageUrl: data.imageUrl.stringValue || '',
+            ingredients: (data.ingredients.arrayValue.values || []).map(
+              (ingredient: { stringValue: string }) =>
+                ingredient.stringValue || ''
+            ),
+            instructions: (data.instructions.arrayValue.values || []).map(
+              (instruction: { stringValue: string }) =>
+                instruction.stringValue || ''
+            ),
+          };
+        });
+      }),
+      catchError((error) => {
+        console.error('Error fetching user recipes:', error);
+        return throwError(
+          'Something went wrong while fetching user recipes. Please try again later.'
+        );
+      })
+    );
   }
 }
