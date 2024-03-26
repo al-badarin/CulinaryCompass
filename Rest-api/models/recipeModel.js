641543e18ddc17c52720f5e544f83917367824ea
al-badarin <jamalalbadarin@gmail.com>
2024-03-26 22:58:48 +0200

    Create recipeModel.js

---
+++ Rest-api/models/recipeModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
+
+const recipeSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    imageUrl: String,
+    ingredients: [String],
+    instructions: [String],
+    userId: {
+      type: ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Recipe", recipeSchema);
+
+// const Recipe = mongoose.model("Recipe", recipeSchema);
+// module.exports = Recipe;
