d813aaf312d55dbbd34a876232f75603df0179d5
al-badarin <jamalalbadarin@gmail.com>
2024-03-26 22:59:06 +0200

    Update index.js

--- Rest-api/router/index.js
+++ Rest-api/router/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const users = require('./users');
+const recipes = require('./recipes');
 const themes = require('./themes');
 const posts = require('./posts');
 const likes = require('./likes');
@@ -11,6 +12,7 @@ router.post('/login', authController.login);
 router.post('/logout', authController.logout);
 
 router.use('/users', users);
+router.use('/recipes', recipes);
 router.use('/themes', themes);
 router.use('/posts', posts);
 router.use('/likes', likes);
