432a253f4f2114a1dd456a5e46bbe9060ed0661e
al-badarin <jamalalbadarin@gmail.com>
2024-03-26 22:59:09 +0200

    Update index.js

--- Rest-api/models/index.js
+++ Rest-api/models/index.js
@@ -1,11 +1,13 @@
-const userModel = require('./userModel');
-const tokenBlacklistModel = require('./tokenBlacklistModel');
-const themeModel = require('./themeModel');
-const postModel = require('./postModel');
+const userModel = require("./userModel");
+const tokenBlacklistModel = require("./tokenBlacklistModel");
+const themeModel = require("./themeModel");
+const postModel = require("./postModel");
+const recipeModel = require("./recipeModel");
 
 module.exports = {
-    userModel,
-    tokenBlacklistModel,
-    themeModel,
-    postModel,
-}
+  userModel,
+  tokenBlacklistModel,
+  themeModel,
+  postModel,
+  recipeModel,
+};
