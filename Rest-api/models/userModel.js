f631d55a08f3373a05d11355cbd68f97473c98c6
al-badarin <jamalalbadarin@gmail.com>
2024-03-26 22:58:57 +0200

    Update userModel.js

--- Rest-api/models/userModel.js
+++ Rest-api/models/userModel.js
@@ -1,74 +1,92 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const saltRounds = Number(process.env.SALTROUNDS) || 5;
 
 const { ObjectId } = mongoose.Schema.Types;
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     tel: {
-        type: String,
+      type: String,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     username: {
-        type: String,
-        required: true,
-        unique: true,
-        minlength: [5, 'Username should be at least 5 characters'],
-        validate: {
-            validator: function (v) {
-                return /[a-zA-Z0-9]+/g.test(v);
-            },
-            message: props => `${props.value} must contains only latin letters and digits!`
+      type: String,
+      required: true,
+      unique: true,
+      minlength: [5, "Username should be at least 5 characters"],
+      validate: {
+        validator: function (v) {
+          return /[a-zA-Z0-9]+/g.test(v);
         },
+        message: (props) =>
+          `${props.value} must contains only latin letters and digits!`,
+      },
     },
     password: {
-        type: String,
-        required: true,
-        minlength: [5, 'Password should be at least 5 characters'],
-        validate: {
-            validator: function (v) {
-                return /[a-zA-Z0-9]+/g.test(v);
-            },
-            message: props => `${props.value} must contains only latin letters and digits!`
+      type: String,
+      required: true,
+      minlength: [5, "Password should be at least 5 characters"],
+      validate: {
+        validator: function (v) {
+          return /[a-zA-Z0-9]+/g.test(v);
         },
+        message: (props) =>
+          `${props.value} must contains only latin letters and digits!`,
+      },
     },
-    themes: [{
+    themes: [
+      {
         type: ObjectId,
-        ref: "Theme"
-    }],
-    posts: [{
+        ref: "Theme",
+      },
+    ],
+    posts: [
+      {
         type: ObjectId,
-        ref: "Post"
-    }]
-}, { timestamps: { createdAt: 'created_at' } });
+        ref: "Post",
+      },
+    ],
+    recipes: [
+      {
+        type: ObjectId,
+        ref: "Recipes",
+      },
+    ],
+  },
+  { timestamps: { createdAt: "created_at" } }
+);
 
 userSchema.methods = {
-    matchPassword: function (password) {
-        return bcrypt.compare(password, this.password);
-    }
-}
+  matchPassword: function (password) {
+    return bcrypt.compare(password, this.password);
+  },
+};
 
-userSchema.pre('save', function (next) {
-    if (this.isModified('password')) {
-        bcrypt.genSalt(saltRounds, (err, salt) => {
-            if (err) {
-                next(err);
-            }
-            bcrypt.hash(this.password, salt, (err, hash) => {
-                if (err) {
-                    next(err);
-                }
-                this.password = hash;
-                next();
-            })
-        })
-        return;
-    }
-    next();
+userSchema.pre("save", function (next) {
+  if (this.isModified("password")) {
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) {
+        next(err);
+      }
+      bcrypt.hash(this.password, salt, (err, hash) => {
+        if (err) {
+          next(err);
+        }
+        this.password = hash;
+        next();
+      });
+    });
+    return;
+  }
+  next();
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
+
+// const User = mongoose.model("User", userSchema);
+// module.exports = User;
