937e2c6600f866d23dcd2200fe61b870ddf44c62
al-badarin <jamalalbadarin@gmail.com>
2024-03-27 14:27:27 +0200

    Update profile.component.ts

--- recipe-sharing-app/src/app/user/profile/profile.component.ts
+++ recipe-sharing-app/src/app/user/profile/profile.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Recipe } from 'src/app/models/recipe.model';
 import { AuthService } from '../auth.service';
 import { RecipeService } from 'src/app/recipe/recipe.service';
-import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-profile',
@@ -10,7 +9,6 @@ import { User } from 'src/app/models/user';
   styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
-  user: User | undefined;
   userRecipes: Recipe[] = [];
 
   constructor(
@@ -19,30 +17,12 @@ export class ProfileComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    const userId = this.authService.getUserId();
+    this.getMyRecipes();
+  }
 
-    if (userId) {
-      // Fetch user's recipes using user ID
-      this.recipeService.getUserRecipesById().subscribe(
-        (recipes: Recipe[]) => {
-          this.userRecipes = recipes;
-          console.log(this.user);
-          console.log(this.userRecipes);
-        },
-        (error) => {
-          console.error('Error fetching user recipes:', error);
-        }
-      );
-
-      // Get user profile data
-      this.authService.getProfile().subscribe(
-        (user) => {
-          this.user = user;
-        },
-        (error) => {
-          console.error('Error fetching user profile:', error);
-        }
-      );
-    }
+  getMyRecipes() {
+    this.recipeService.getMyRecipes().subscribe((recipes) => {
+      this.userRecipes = recipes;
+    });
   }
 }
