d8d46dc0f3c8570322b4c6be46352e66e4b13386
al-badarin <jamalalbadarin@gmail.com>
2024-03-28 01:45:32 +0200

    Update profile.component.ts

--- recipe-sharing-app/src/app/user/profile/profile.component.ts
+++ recipe-sharing-app/src/app/user/profile/profile.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Recipe } from 'src/app/models/recipe.model';
 import { AuthService } from '../auth.service';
 import { RecipeService } from 'src/app/recipe/recipe.service';
+import { Profile } from 'src/app/models/profile';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -10,13 +13,29 @@ import { RecipeService } from 'src/app/recipe/recipe.service';
 })
 export class ProfileComponent implements OnInit {
   recipes: Recipe[] = [];
+  profileDetails: Profile | undefined;
+  isEditMode: boolean = false;
+
+  form = this.fb.group({
+    username: ['', [Validators.required, Validators.minLength(5)]],
+    email: ['', [Validators.required, Validators.email]],
+  });
 
   constructor(
     private authService: AuthService,
-    private recipeService: RecipeService
+    private recipeService: RecipeService,
+    private fb: FormBuilder,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
+    // Setting USER'S FIELDS
+    const { username, email } = this.authService.user!;
+
+    this.profileDetails = { username, email };
+    this.form.setValue({ username, email });
+
+    // Getting USER'S RECIPES
     this.getMyRecipes();
   }
 
@@ -24,5 +43,41 @@ export class ProfileComponent implements OnInit {
     this.recipeService.getMyRecipes().subscribe((recipes) => {
       this.recipes = recipes;
     });
+  }
+
+  toggleEditMode(): void {
+    this.isEditMode = !this.isEditMode;
+  }
+
+  saveProfileHandle(): void {
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.profileDetails = { ...this.form.value } as Profile;
+    const { username, email } = this.profileDetails;
+
+    this.authService.updateProfile(username!, email!).subscribe(() => {
+      this.toggleEditMode();
+    });
+  }
+
+  onCancel(e: Event): void {
+    e.preventDefault();
+    this.toggleEditMode();
+  }
+
+  onDeleteRecipe(recipeId: string): void {
+    console.log('delete btn clicked');
+
+    this.recipeService.removeRecipe(recipeId).subscribe(
+      () => {
+        this.router.navigate(['/auth/profile']);
+      },
+      (error) => {
+        console.error('Deleting Recipe Error:', error);
+        this.router.navigateByUrl('/auth/profile');
+      }
+    );
   }
 }
