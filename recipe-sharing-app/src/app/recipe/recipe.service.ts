ed9a172e552905603d13ee33cc5ccc7d582ba071
al-badarin <jamalalbadarin@gmail.com>
2024-03-27 14:27:49 +0200

    Update recipe.service.ts

--- recipe-sharing-app/src/app/recipe/recipe.service.ts
+++ recipe-sharing-app/src/app/recipe/recipe.service.ts
@@ -3,49 +3,17 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { Recipe } from '../models/recipe.model';
-import { FirestoreRecipe } from '../models/FirestoreRecipe.model';
 import { AuthService } from '../user/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RecipeService {
-  private baseUrl =
-    'https://firestore.googleapis.com/v1/projects/recipe-sharing-app-al-badarin/databases/(default)/documents/recipes';
-
   constructor(private http: HttpClient, private authService: AuthService) {}
 
   // GET ALL RECIPES
-  getRecipes(): Observable<Recipe[]> {
-    return this.http.get<FirestoreRecipe>(this.baseUrl).pipe(
-      map((response) => {
-        if (
-          !response ||
-          !response.documents ||
-          !Array.isArray(response.documents)
-        ) {
-          console.error('Invalid response format:', response);
-          return [];
-        }
-
-        return response.documents.map((doc: FirestoreRecipe) => {
-          const data = doc.fields;
-          return {
-            id: doc.name.split('/').pop() || '',
-            title: data.title.stringValue || '',
-            description: data.description.stringValue || '',
-            imageUrl: data.imageUrl.stringValue || '',
-            ingredients: (data.ingredients.arrayValue.values || []).map(
-              (ingredient: { stringValue: string }) =>
-                ingredient.stringValue || ''
-            ),
-            instructions: (data.instructions.arrayValue.values || []).map(
-              (instruction: { stringValue: string }) =>
-                instruction.stringValue || ''
-            ),
-          };
-        });
-      }),
+  getRecipes() {
+    return this.http.get<Recipe[]>(`/api/recipes`).pipe(
       catchError((error) => {
         console.error('Error fetching recipes:', error);
         return throwError(
@@ -56,116 +24,35 @@ export class RecipeService {
   }
 
   // GET RECIPE'S DETAILS
-  getRecipeDetails(recipeId: string): Observable<Recipe | undefined> {
-    const url = `${this.baseUrl}/${recipeId}`;
-    // return this.http.get<Recipe>(url);
+  getRecipeDetails(recipeId: string) {
+    const url = `/api/recipes/${recipeId}`;
 
-    return this.http.get<FirestoreRecipe>(url).pipe(
-      map((response) => {
-        const data = response.fields;
-        return {
-          id: response.name.split('/').pop() || '',
-          title: data.title.stringValue || '',
-          description: data.description.stringValue || '',
-          imageUrl: data.imageUrl.stringValue || '',
-          ingredients: (data.ingredients.arrayValue.values || []).map(
-            (ingredient: { stringValue: string }) =>
-              ingredient.stringValue || ''
-          ),
-          instructions: (data.instructions.arrayValue.values || []).map(
-            (instruction: { stringValue: string }) =>
-              instruction.stringValue || ''
-          ),
-        };
-      }),
+    return this.http.get<Recipe>(url).pipe(
       catchError((error) => {
-        console.error('Error fetching recipes:', error);
+        console.error('Error fetching recipe details:', error);
         return throwError(
-          'Something went wrong while fetching recipes. Please try again later.'
+          'Something went wrong while fetching recipe details. Please try again later.'
         );
       })
     );
   }
 
   // ADD A RECIPE
-  addRecipe(recipe: Recipe): Observable<Recipe> {
-    return this.http
-      .post<Recipe>(this.baseUrl, {
-        fields: {
-          title: { stringValue: recipe.title },
-          description: { stringValue: recipe.description },
-          imageUrl: { stringValue: recipe.imageUrl },
-          ingredients: {
-            arrayValue: {
-              values: recipe.ingredients.map((ingredient) => ({
-                stringValue: ingredient,
-              })),
-            },
-          },
-          instructions: {
-            arrayValue: {
-              values: recipe.instructions.map((instruction) => ({
-                stringValue: instruction,
-              })),
-            },
-          },
-        },
-      })
-      .pipe(
-        catchError((error) => {
-          console.error('Error adding recipe:', error);
-          return throwError(
-            'Something went wrong while adding the recipe. Please try again later.'
-          );
-        })
-      );
-  }
+  addRecipe(recipeData: Object) {
+    // const { apiUrl } = environment;
 
-  // GET USER'S RECIPES BY ID
-  getUserRecipesById(): Observable<Recipe[]> {
-    const userId = this.authService.getUserId();
-
-    if (!userId) {
-      return throwError('User ID not found');
-    }
-
-    const url = `${this.baseUrl}?q=userId:${userId}`;
-
-    return this.http.get<FirestoreRecipe>(url).pipe(
-      map((response) => {
-        if (
-          !response ||
-          !response.documents ||
-          !Array.isArray(response.documents)
-        ) {
-          console.error('Invalid response format:', response);
-          return [];
-        }
-
-        return response.documents.map((doc: FirestoreRecipe) => {
-          const data = doc.fields;
-          return {
-            id: doc.name.split('/').pop() || '',
-            title: data.title.stringValue || '',
-            description: data.description.stringValue || '',
-            imageUrl: data.imageUrl.stringValue || '',
-            ingredients: (data.ingredients.arrayValue.values || []).map(
-              (ingredient: { stringValue: string }) =>
-                ingredient.stringValue || ''
-            ),
-            instructions: (data.instructions.arrayValue.values || []).map(
-              (instruction: { stringValue: string }) =>
-                instruction.stringValue || ''
-            ),
-          };
-        });
-      }),
+    return this.http.post<Recipe>(`/api/recipes`, recipeData).pipe(
       catchError((error) => {
-        console.error('Error fetching user recipes:', error);
+        console.error('Error adding recipe:', error);
         return throwError(
-          'Something went wrong while fetching user recipes. Please try again later.'
+          'Something went wrong while adding the recipe. Please try again later.'
         );
       })
     );
+  }
+
+  // GET ALL USER'S RECIPES
+  getMyRecipes() {
+    return this.http.get<Recipe[]>(`/api/recipes/my-recipes`);
   }
 }
