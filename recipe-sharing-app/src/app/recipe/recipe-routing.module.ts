834440982ced6d1beb718217af35fe6d90d27e20
al-badarin <jamalalbadarin@gmail.com>
2024-03-28 15:29:22 +0200

    Update recipe-routing.module.ts

--- recipe-sharing-app/src/app/recipe/recipe-routing.module.ts
+++ recipe-sharing-app/src/app/recipe/recipe-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 import { AddRecipeComponent } from './add-recipe/add-recipe.component';
+import { EditRecipeComponent } from './edit-recipe/edit-recipe.component';
 // import { AuthActivate } from '../core/guards/auth.activate';
 
 const routes: Routes = [
@@ -14,12 +15,15 @@ const routes: Routes = [
     path: 'details/:recipeId',
     component: RecipeDetailsComponent,
   },
-  //   TODO:
-  // *add paths to 'recipes/:id/edit'
-  // *add paths to 'recipes/:id/delete'
   {
     path: 'add-recipe',
     component: AddRecipeComponent,
+    //   TODO:
+    // canActivate: [AuthActivate]
+  },
+  {
+    path: 'edit/:recipeId',
+    component: EditRecipeComponent,
     //   TODO:
     // canActivate: [AuthActivate]
   },
