ad71488f3691e741efd683e8cd573314c7e9d59b
al-badarin <jamalalbadarin@gmail.com>
2024-03-28 23:23:43 +0200

    recipes sorted by recent date

--- recipe-sharing-app/src/app/recipe/recipe-list/recipe-list.component.ts
+++ recipe-sharing-app/src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { RecipeService } from '../recipe.service';
 import { Recipe } from '../../models/recipe.model';
- 
+import { UtilsService } from 'src/app/shared/utils.service';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -10,12 +11,15 @@ import { Recipe } from '../../models/recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
 
-  constructor(private recipeService: RecipeService) {}
+  constructor(
+    private recipeService: RecipeService,
+    private utilsService: UtilsService
+  ) {}
 
   ngOnInit(): void {
     this.recipeService.getRecipes().subscribe({
       next: (recipes: Recipe[]) => {
-        this.recipes = recipes;
+        this.recipes = this.utilsService.sortRecipesByDate(recipes);
       },
       error: (error) => {
         console.error('Error fetching recipes:', error);
