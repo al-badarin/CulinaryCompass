a055b5f3708639e95764c5b75f248b832263540b
al-badarin <jamalalbadarin@gmail.com>
2024-03-28 15:29:19 +0200

    Update recipe-details.component.ts

--- recipe-sharing-app/src/app/recipe/recipe-details/recipe-details.component.ts
+++ recipe-sharing-app/src/app/recipe/recipe-details/recipe-details.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../../models/recipe.model';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RecipeService } from '../recipe.service';
+import { AuthService } from 'src/app/user/auth.service';
+import { User } from 'src/app/models/user';
+import { UserId } from 'src/app/models/user-id';
 
 @Component({
   selector: 'app-recipe-details',
@@ -10,14 +13,28 @@ import { RecipeService } from '../recipe.service';
 })
 export class RecipeDetailsComponent implements OnInit {
   recipe: Recipe | undefined;
-  ingredients: string | undefined;
+  isLoggedIn: boolean = false;
+  isOwner: boolean = false;
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private recipeService: RecipeService
+    private recipeService: RecipeService,
+    private authService: AuthService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
+    this.authService.user$.subscribe((user) => {
+      if (user) {
+        this.isLoggedIn = true;
+        if (this.recipe) {
+          this.checkIsOwner(user._id);
+        }
+      } else {
+        this.isLoggedIn = false;
+      }
+    });
+
     this.fetchRecipeDetails();
   }
 
@@ -28,11 +45,33 @@ export class RecipeDetailsComponent implements OnInit {
       this.recipeService.getRecipeDetails(id).subscribe(
         (recipe) => {
           this.recipe = recipe;
+          if (this.isLoggedIn && this.recipe) {
+            this.checkIsOwner(this.recipe.userId._id);
+          }
         },
         (error) => {
           console.error('Error fetching recipe details:', error);
         }
       );
+    }
+  }
+
+  checkIsOwner(recipeUserId: string): void {
+    const user = this.authService.user;
+    if (user && user._id === recipeUserId) {
+      this.isOwner = true;
+    } else {
+      this.isOwner = false;
+    }
+  }
+
+  canEditRecipe(): boolean {
+    return this.isLoggedIn && this.isOwner;
+  }
+
+  editRecipe(): void {
+    if (this.recipe) {
+      this.router.navigate(['/recipes/edit', this.recipe._id]);
     }
   }
 }
