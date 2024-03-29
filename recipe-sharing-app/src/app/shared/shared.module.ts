268befbb64cc2065c133e6341f969a09bbd6301b
al-badarin <jamalalbadarin@gmail.com>
2024-03-30 02:03:52 +0300

    Update shared.module.ts

--- recipe-sharing-app/src/app/shared/shared.module.ts
+++ recipe-sharing-app/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SlicePipe } from './pipes/slice.pipe';
+import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
-  declarations: [SlicePipe],
+  declarations: [SlicePipe, LoaderComponent],
   imports: [CommonModule],
-  exports: [SlicePipe],
+  exports: [SlicePipe, LoaderComponent],
 })
 export class SharedModule {}
