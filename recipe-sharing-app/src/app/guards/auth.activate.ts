cb1f1a63e892bc1b38c4451db28be2c1d666a740
al-badarin <jamalalbadarin@gmail.com>
2024-03-30 02:04:02 +0300

    Create auth.activate.ts

---
+++ recipe-sharing-app/src/app/guards/auth.activate.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/user/auth.service';
+
+@Injectable({ providedIn: 'root' })
+export class AuthActivate implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    if (this.authService.isLoggedIn) {
+      return true;
+    } else {
+      return this.router.createUrlTree(['/auth/login']);
+    }
+  }
+}
