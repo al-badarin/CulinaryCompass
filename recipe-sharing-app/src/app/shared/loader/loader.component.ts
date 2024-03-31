752473a1af25848b78b41da46e7dfe1a88bedcf9
al-badarin <jamalalbadarin@gmail.com>
2024-03-31 03:16:30 +0300

    Update loader.component.ts

--- recipe-sharing-app/src/app/shared/loader/loader.component.ts
+++ recipe-sharing-app/src/app/shared/loader/loader.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { LoaderService } from './loader.service';
 
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.component.html',
   styleUrls: ['./loader.component.css'],
 })
-export class LoaderComponent {}
+export class LoaderComponent {
+  // @Input() isLoading: boolean = false;
+
+  constructor(public spinnerService: LoaderService) {}
+}
+ 
