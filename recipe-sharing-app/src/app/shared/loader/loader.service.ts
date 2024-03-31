0fdb26ef27f7233c5ed2bda6aeb843fee84710b4
al-badarin <jamalalbadarin@gmail.com>
2024-03-31 03:16:26 +0300

    Create loader.service.ts

---
+++ recipe-sharing-app/src/app/shared/loader/loader.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoaderService {
+  // private isLoadingSubject = new BehaviorSubject<boolean>(false);
+  // isLoading$ = this.isLoadingSubject.asObservable();
+
+  // show() {
+  //   this.isLoadingSubject.next(true);
+  // }
+
+  // hide() {
+  //   this.isLoadingSubject.next(false);
+  // }
+  
+ isLoading: boolean = false;
+
+  show() {
+    this.isLoading = true;
+  }
+
+  hide() {
+    this.isLoading = false;
+  }
+}
