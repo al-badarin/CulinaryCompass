f6f0f47916a8b454be2bb3d103e5fcde2fd4a1de
al-badarin <jamalalbadarin@gmail.com>
2024-03-31 03:17:03 +0300

    Update app.component.ts

--- recipe-sharing-app/src/app/app.component.ts
+++ recipe-sharing-app/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { LoaderService } from './shared/loader/loader.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'my-app-project';
+
+  constructor(public loaderService: LoaderService) {}
+
+  get isLoading(): boolean {
+    return this.loaderService.isLoading;
+  }
+
+  // isLoading = false;
+
+  // constructor(private spinnerService: SpinnerService) {
+  //   this.spinnerService.isLoading$.subscribe((isLoading: boolean) => {
+  //     this.isLoading = isLoading;
+  //   });
+  // }
 }
