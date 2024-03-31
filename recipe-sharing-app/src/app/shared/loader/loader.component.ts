0e4976215ab015d4c4f3bf5e1470fba2c71d7eae
al-badarin <jamalalbadarin@gmail.com>
2024-04-01 01:14:36 +0300

    Update loader.component.ts

--- recipe-sharing-app/src/app/shared/loader/loader.component.ts
+++ recipe-sharing-app/src/app/shared/loader/loader.component.ts
@@ -1,14 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { LoaderService } from './loader.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.component.html',
   styleUrls: ['./loader.component.css'],
 })
-export class LoaderComponent {
-  // @Input() isLoading: boolean = false;
+export class LoaderComponent implements OnDestroy {
+  isLoading: boolean = false;
+  private subscription: Subscription;
 
-  constructor(public spinnerService: LoaderService) {}
+  constructor(public loaderService: LoaderService) {
+    this.subscription = this.loaderService.isLoading.subscribe((isLoading) => {
+      this.isLoading = isLoading;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
- 
