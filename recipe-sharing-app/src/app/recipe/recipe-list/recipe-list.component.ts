0e891830875da7dd18d4edca32e50eb14f8d3da1
al-badarin <jamalalbadarin@gmail.com>
2024-04-03 12:52:54 +0300

    Update recipe-list.component.ts

--- recipe-sharing-app/src/app/recipe/recipe-list/recipe-list.component.ts
+++ recipe-sharing-app/src/app/recipe/recipe-list/recipe-list.component.ts
@@ -10,6 +10,10 @@ import { UtilsService } from 'src/app/shared/utils.service';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
+  displayedRecipes: Recipe[] = []; // Add this line
+
+  currentPage: number = 1;
+  itemsPerPage: number = 6; // Choose your desired number of items per page
 
   constructor(
     private recipeService: RecipeService,
@@ -20,10 +24,28 @@ export class RecipeListComponent implements OnInit {
     this.recipeService.getRecipes().subscribe({
       next: (recipes: Recipe[]) => {
         this.recipes = this.utilsService.sortRecipesByDate(recipes);
+        this.updateDisplayedRecipes(); // Update displayed recipes on init
       },
       error: (error) => {
         console.error('Error fetching recipes:', error);
       },
     });
+  }
+
+  onPageChange(page: number): void {
+    this.currentPage = page;
+    this.updateDisplayedRecipes();
+  }
+  
+  private updateDisplayedRecipes(): void {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.displayedRecipes = this.recipes.slice(startIndex, endIndex);
+  }
+
+  get totalPages(): number[] {
+    const totalRecipes = this.recipes.length;
+    const totalPages = Math.ceil(totalRecipes / this.itemsPerPage);
+    return Array.from({ length: totalPages }, (_, index) => index + 1);
   }
 }
