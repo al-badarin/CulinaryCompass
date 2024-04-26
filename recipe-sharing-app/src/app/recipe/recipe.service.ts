ee75774291f5b7ecd58914031d67cc92bf47bd04
al-badarin <jamalalbadarin@gmail.com>
2024-04-26 23:31:19 +0300

    added isOwnerOfRecipe() method

--- recipe-sharing-app/src/app/recipe/recipe.service.ts
+++ recipe-sharing-app/src/app/recipe/recipe.service.ts
@@ -1,14 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { throwError } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { Recipe } from '../models/recipe.model';
+import { AuthService } from '../user/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RecipeService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private authService: AuthService) {}
+  // Check if the current user is the owner of a recipe
+  isOwnerOfRecipe(recipeUserId: string): Observable<boolean> {
+    return this.authService.user$.pipe(
+      map((user) => {
+        if (!user) {
+          // If the user is not logged in, return false
+          return false;
+        }
+        // Check if the current user ID matches the recipe user ID
+        return user._id === recipeUserId;
+      }),
+      catchError((error) => {
+        console.error('Error checking ownership:', error);
+        return throwError(false);
+      })
+    );
+  }
 
   // GET ALL RECIPES
   getRecipes() {
